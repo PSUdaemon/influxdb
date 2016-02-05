9799996c16f240dd0105b9c0c6eeb76f7455e2db
David Norton <dgnorton@gmail.com>
2016-02-05 09:27:10 -0500

    fix build after rebase on master

--- cmd/influxd/run/server.go
+++ cmd/influxd/run/server.go
@@ -124,7 +124,7 @@ func NewServer(c *Config, buildInfo *BuildInfo) (*Server, error) {
 	}
 
 	// Create the root directory if it doesn't already exist.
-	if err := os.MkdirAll(dir, 0777); err != nil {
+	if err := os.MkdirAll(c.Meta.Dir, 0777); err != nil {
 		return nil, fmt.Errorf("mkdir all: %s", err)
 	}
 
